fe9ab6b1ac0a369be0e5c70aecb4ee8a9f1ff02a
Alex28345 <114405664+Alex28345@users.noreply.github.com>
2024-02-13 17:27:47 +0100

    bug

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
-import {Controller} from "./controllers/";
-import {TemperatureObservable} from "./models/";
-import {Graph} from "./view/";
+import {Controller} from "./controllers/index.js";
+import {TemperatureObservable} from "./models/index.js";
+import {Graph} from "./view/index.js";
 
 let controller = new Controller();
 let temperature = new TemperatureObservable();
@@ -8,10 +8,7 @@ temperature.addObserver(controller);
 
 document.getElementById("dataButton").onclick = function() {
     document.getElementById('graph').style.display = 'none';
-    document.getElementById('data').style.display = 'flex'; 
-    const data = [0,4,8,20,2,5,6,3,9,10,5,15];
-    const graph = new Graph('lineChart');
-    graph.drawGraph(data);
+    document.getElementById('data').style.display = 'flex';
 };
 
 document.getElementById("graphButton").onclick = function() {
