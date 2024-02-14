5932e889f0d7fed80f941fca3cc5200f8c9467c4
Alex28345 <114405664+Alex28345@users.noreply.github.com>
2024-02-14 01:34:17 +0100

    bouton d'ajout

--- src/script.js
+++ src/script.js
@@ -18,9 +18,47 @@ let dashboard = new Dashboard();
 // };
 //
 document.getElementById("ongletHistory").onclick = () => {
-
     const data = [0, 4, 8, 20, 2, -5, 6, 3, 9, 10, 5, 15];
     const graph = new Graph('lineChart');
     graph.drawGraph(data);
 };
 
+
+
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+        registration.unregister();
+    }
+});
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/src/serviceWorker.js')
+        .then(() => { console.log('Service Worker Registered'); });
+}
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+
+    addBtn.addEventListener('click', (e) => {
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
