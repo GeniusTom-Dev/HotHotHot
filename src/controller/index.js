924f5277ff70ce04546e5b4d8f854a12c1f49aa9
Alex28345 <114405664+Alex28345@users.noreply.github.com>
2024-02-18 22:55:51 +0100

    CLEAR

--- src/controller/index.js
+++
@@ -1,7 +0,0 @@
-import {HomeController} from "./HomeController.js";
-import {DocsController} from "./DocsController.js";
-
-export {
-    HomeController,
-    DocsController
-}
--- src/script.js
+++
@@ -1,6 +0,0 @@
-import {Router} from "./control/Router.js";
-
-const router = new Router();
-
-let path = window.location.pathname;
-router.showRoute(path)
